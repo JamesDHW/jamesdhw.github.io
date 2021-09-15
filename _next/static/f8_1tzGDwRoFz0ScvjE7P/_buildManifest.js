450ae00cc39e9ccbaebd9cfc4b8b1287f6f9c43e
James Haworth Wheatman <JamesHaworthWheatman@gmail.com>
2021-09-15 23:48:17 +0100

    Updates

--- _next/static/f8_1tzGDwRoFz0ScvjE7P/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/6278397b-023416bf54cc5d966d35.js","static/chunks/35-0c46795aedac5d42fa6a.js","static/chunks/pages/index-4674bd340b878b856269.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
